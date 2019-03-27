9dfe55022528d1e921ec94c37eed2bafb22fe6b3
Ruzgar Zere <rzere@rollins.edu>
2019-03-27 11:53:10 -0400

    TODO 2

--- Project3_AutoStitch/alignment.py
+++ Project3_AutoStitch/alignment.py
@@ -42,7 +42,25 @@ def computeHomography(f1, f2, matches, A_out=None):
         #Fill in the matrix A in this loop.
         #Access elements using square brackets. e.g. A[0,0]
         #TODO-BLOCK-BEGIN
-        raise Exception("TODO in alignment.py not implemented")
+        A[2 * i, 0] = a_x
+        A[2 * i, 1] = a_y
+        A[2 * i, 2] = 1
+        A[2 * i, 3] = 0
+        A[2 * i, 4] = 0
+        A[2 * i, 5] = 0
+        A[2 * i, 6] = -(b_x*a_x)
+        A[2 * i, 7] = -(b_x*a_y)
+        A[2 * i, 8] = -b_x
+        A[2 * i + 1, 0] = 0
+        A[2 * i + 1, 1] = 0
+        A[2 * i + 1, 2] = 0
+        A[2 * i + 1, 3] = a_x
+        A[2 * i + 1, 4] = a_y
+        A[2 * i + 1, 5] = 1
+        A[2 * i + 1, 6] = -(b_y*a_x)
+        A[2 * i + 1, 7] = -(b_y*a_x)
+        A[2 * i + 1, 8] = -(b_y*a_x)
+
         #TODO-BLOCK-END
         #END TODO
 
@@ -62,7 +80,7 @@ def computeHomography(f1, f2, matches, A_out=None):
     #BEGIN TODO 3
     #Fill the homography H with the appropriate elements of the SVD
     #TODO-BLOCK-BEGIN
-    raise Exception("TODO in alignment.py not implemented")
+
     #TODO-BLOCK-END
     #END TODO
 

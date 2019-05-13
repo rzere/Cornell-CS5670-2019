fe4df0627f96c8af221f129b7ba5f258a5a67eb8
Ruzgar Zere <rzere@rollins.edu>
2019-05-13 16:47:16 -0400

    added student.py final

--- Project4_Stereo/student.py
+++ Project4_Stereo/student.py
@@ -4,7 +4,7 @@ import time
 from math import floor
 import numpy as np
 import cv2
-from scipy.sparse import csr_matrix
+from scipy import linalg
 # import util_sweep
 # END IMPORTS
 
@@ -21,7 +21,7 @@ def compute_photometric_stereo_impl(lights, images):
     Normals should be unit vectors.
 
     Input:
-        lights -- N x 3 array.  Rows are normalized and are to be interpreted
+        lights -- 3 x N array.  Rows are normalized and are to be interpreted
                   as lighting directions.
         images -- list of N images.  Each image is of the same scene from the
                   same viewpoint, but under the lighting condition specified in
@@ -32,14 +32,32 @@ def compute_photometric_stereo_impl(lights, images):
         normals -- float32 height x width x 3 image with dimensions matching
                    the input images.
     """
-    raise NotImplementedError()
+    height, width, rgb = images[0].shape[0], images[0].shape[1], images[0].shape[2]
+    albedo = np.zeros((height, width, rgb), dtype=np.uint8)
+    normals = np.zeros((height, width, 3), dtype=np.float32)
+    n_images = len(images)
+    L = np.array(lights)
+    for i in range(height):
+        for j in range(width):
+            for k in range(rgb):
+                I = np.array([image[i, j, k] for image in images])
+                I.reshape((n_images, 1))
+                G = np.dot(linalg.inv(np.dot(L.T, L)), np.dot(L.T, I))
+                kd = np.linalg.norm(G)
+                if kd < 1e-7:
+                    kd, N = 0, np.zeros((G.shape[0]))
+                else:
+                    N = G / kd
+                albedo[i, j, k] = kd
+                normals[i, j] = N.reshape((3,))
 
-
+    return albedo, normals
 
 
 def project_impl(K, Rt, points):
     """
     Project 3D points into a calibrated camera.
+
     Input:
         K -- camera intrinsics calibration matrix
         Rt -- 3 x 4 camera extrinsics calibration matrix
@@ -47,8 +65,19 @@ def project_impl(K, Rt, points):
     Output:
         projections -- height x width x 2 array of 2D projections
     """
-    raise NotImplementedError()
+    height, width = points.shape[0], points.shape[1]
+    projections = np.zeros((height, width, 2))
 
+    krt = np.dot(K, Rt)
+
+    for i in range(height):
+        for j in range(width):
+            p = points[i,j]
+            p = np.append(p, 1)
+            p_ = np.dot(krt, p)
+            projections[i,j,0] = p_[0] / p_[2]
+            projections[i,j,1] = p_[1] / p_[2]
+    return projections
 
 
 def preprocess_ncc_impl(image, ncc_size):
@@ -90,9 +119,7 @@ def preprocess_ncc_impl(image, ncc_size):
     +------+------+  +------+------+  v
     width ------->
 
-    v = [ x111, x121, x211, x112, x112, x122, x212, x222 ]
-
-    see order argument in np.reshape
+    v = [ x111, x112, x121, x122, x211, x212, x221, x222 ]
 
     Input:
         image -- height x width x channels image of type float32
@@ -100,7 +127,27 @@ def preprocess_ncc_impl(image, ncc_size):
     Output:
         normalized -- heigth x width x (channels * ncc_size**2) array
     """
-    raise NotImplementedError()
+    height, width, rgb = image.shape
+    normalized = np.zeros((height, width, rgb * ncc_size ** 2), dtype=np.float32)
+    ncc_h = ncc_size / 2
+
+    for i in range(height):
+        for j in range(width):
+            if i - ncc_h < 0 or i + ncc_h >= height or j - ncc_h < 0 or j + ncc_h >= width:
+                continue
+            temp = []
+            vec = []
+            for c in range(rgb):
+                patch = image[i-ncc_h:i+ncc_h+1, j-ncc_h:j+ncc_h+1, c]
+                patch = (patch - np.mean(patch)).flatten()
+                temp = temp + patch.tolist()
+            n = np.linalg.norm(vec)
+            if n < 1e-6:
+                vec = np.zeros(rgb * ncc_size ** 2)
+            else:
+                vec = vec / n
+            normalized[i, j] = vec
+    return normalized
 
 
 def compute_ncc_impl(image1, image2):
@@ -115,4 +162,9 @@ def compute_ncc_impl(image1, image2):
         ncc -- height x width normalized cross correlation between image1 and
                image2.
     """
-    raise NotImplementedError()
+    height, width = image1.shape[0], image1.shape[1]
+    ncc = np.zeros((height, width))
+    for i in range(height):
+        for j in range(width):
+            ncc[i,j] = np.correlate(image1[i, j], image2[i, j])[0]
+    return ncc
